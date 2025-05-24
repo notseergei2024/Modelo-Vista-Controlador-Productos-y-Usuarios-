fea353860fab90c54bcf461b186726d22776f467
notseergei2024 <jsergiovargas24@gmail.com>
2025-05-24 19:25:36 +0100

    proyecto vista controlador beta 1
    En esta versión solo se pueden agregar usuarios y visualizar lo que está en la base de datos

---
+++ MVC_ProductosUsuarios/src/main/java/com/mycompany/mvc_productosusuarios/MVC_ProductosUsuarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mvc_productosusuarios;
+
+/**
+ *
+ * @author storm
+ */
+public class MVC_ProductosUsuarios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
